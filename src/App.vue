3a05b57f42dbb6d7e462fa7dbb045eefa67e2198
ldmfabio <ldmfabio@gmail.com>
2026-04-28 08:14:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+  import { ref } from 'vue'
+  import { listaProdutos } from './data/produtos';
+  import ProdutoChild from './components/ProdutoChild.vue';
+  import ButtonChild from './components/ButtonChild.vue';
+  const produtos = ref(listaProdutos)
+
+  const alterando = ref(true)
+  const preco = ref(0)
+  const posicaoProduto = ref(-1)
+  function salvarPreco() {
+    alert('Teste')
+  }
+
+  function corrigirPreco(idProduto) {
+    posicaoProduto.value = produtos.value.findIndex(p => p.id === idProduto);
+    preco.value = produtos.value[posicaoProduto.value].preco
+    alterando.value = true;
+  }
+</script>
+
+<template>
+  <div class="container">
+    <h1>Catálogo de Produtos</h1>
+    <div>
+      <ul>
+        <ProdutoChild v-for="produto in produtos"
+            :key="produto.id" :id="produto.id"
+            :nome="produto.nome" :preco="produto.preco"
+            :categoria="produto.categoria"
+            @corrigirpreco="corrigirPreco"
+          >
+        </ProdutoChild>
+      </ul>
+    </div>
+    <div v-show="alterando">
+      <label>Preço</label>
+      <input type="number" v-model.number="preco">
+      <ButtonChild @clique="salvarPreco()">Salvar</ButtonChild>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,18 @@
+<script setup>
+  defineProps(['id', 'nome', 'preco', 'categoria'])
+  import ButtonChild from './ButtonChild.vue';
+  defineEmits(['corrigirpreco'])
+</script>
+
+<template>
+  <li>
+    {{ nome }} - R$ {{ preco }} ({{ categoria }})
+    <ButtonChild @clique="$emit('corrigirpreco', id)">
+      Corrigir Preço
+    </ButtonChild>
+  </li>
+</template>
+
+<style scoped>
+
+</style>
